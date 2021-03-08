b3b12af6b7c762927915b5d91efb3407efe00b8f
Panchis7u7 <smadrigal.rod@gmail.com>
2021-03-07 18:43:48 -0600

    Getting physical devices.

--- app/src/main/cpp/vulkan-utils.cpp
+++ app/src/main/cpp/vulkan-utils.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "vulkan-utils.h"
-#include <vector>
+#include <cstring>
 #include <android/log.h>
 
 VulkanApplication::VulkanApplication() {
@@ -17,27 +17,23 @@ VulkanApplication::~VulkanApplication() {
 std::string VulkanApplication::run() {
     vulkanInstance = VK_NULL_HANDLE;
     std::string message = "";
-    message += vulkanInit();
+    //Create instance, extension support and validation layers.
+    message += createInstance();
+    //Check extension support.
+    message += extensionSupport();
+    //Pick a physical device.
+    message += pickPhysicalDevice();
     return message;
 }
 
-std::string VulkanApplication::extensionSupport() {
-    std::string ext = "";
-    uint32_t extensionCount = 0;
-    //Request del numero de extensiones disponibles.
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    std::vector<VkExtensionProperties> extensiones(extensionCount);
-    //extensiones.reserve(extensionCount);
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensiones.data());
-    for(const auto& extension : extensiones){
-        ext += "\n" + std::string(extension.extensionName) + "\n";
-    }
-    return ext;
-}
 
-std::string VulkanApplication::vulkanInit(){
-    //const char* instanceLayers[] = {"VK_LUNARG_standard_validation"};
-    //const char* instanceExtensions[] = {"VK_EXT_debug_report"};
+//--------------------------------------------------------------------------------------------------
+//Instance and physical device selection.
+
+std::string VulkanApplication::createInstance(){
+    if(enableValidationLayers && !checkValidationSupport()){
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
 
     VkApplicationInfo appInfo = {};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -52,15 +48,103 @@ std::string VulkanApplication::vulkanInit(){
     createInfo.pApplicationInfo = &appInfo;
     createInfo.enabledExtensionCount =0;
     createInfo.ppEnabledExtensionNames = NULL;
+    //createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+    //createInfo.ppEnabledLayerNames = validationLayers.data();
     createInfo.enabledLayerCount = 0;
     createInfo.ppEnabledLayerNames = NULL;
 
-    std::string msg = extensionSupport();
+    //Check validation layers.
+    /*VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        //populateDebugMessengerCreateInfo(debugCreateInfo);
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debugCreateInfo;
+    } else {
+        createInfo.enabledLayerCount = 0;
+        createInfo.pNext = nullptr;
+    }*/
 
     if(vkCreateInstance(&createInfo, nullptr, &vulkanInstance) == VK_SUCCESS){
-        return msg+"Success!";
+        return "Success at creating instance!\n";
     } else {
-        return msg+"Error!";
+        return "Error!\n";
     }
 
 }
+
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+//Extension support.
+
+std::string VulkanApplication::extensionSupport() {
+    std::string ext = "";
+    uint32_t extensionCount = 0;
+    //Request del numero de extensiones disponibles.
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensiones(extensionCount);
+    //extensiones.reserve(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensiones.data());
+    for(const auto& extension : extensiones){
+        ext += "\t" + std::string(extension.extensionName) + "\n";
+    }
+    return ext;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+//Validation Layers (TODO)
+
+bool VulkanApplication::checkValidationSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for(const char* layerName : validationLayers){
+        bool layerFound = false;
+        for(const auto& layerProperties : availableLayers){
+            if(strcmp(layerName, layerProperties.layerName) == 0){
+                layerFound = true;
+                break;
+            }
+        }
+        if(layerFound)
+            return true;
+    }
+    return false;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------------------
+//Pick a physical device.
+
+std::string VulkanApplication::pickPhysicalDevice() {
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    uint32_t deviceCount = 0;
+    if (VK_SUCCESS != vkEnumeratePhysicalDevices(vulkanInstance, &deviceCount, NULL) || deviceCount == 0) {
+        std::cout << "Failed searching for Vulkan physical devices\n";
+        return "No device found!\n";
+    }
+
+    std::vector<VkPhysicalDevice> physical_devices;
+    physical_devices.reserve(deviceCount);
+    if (VK_SUCCESS != vkEnumeratePhysicalDevices(vulkanInstance, &deviceCount, physical_devices.data()) ||
+        deviceCount == 0) {
+        std::cout << "Failed enumerating Vulkan physical devices\n";
+        return "Failed enumerating Vulkan physical devices\n";
+    }
+
+    return "nel";
+
+    if(physicalDevice == VK_NULL_HANDLE){
+        return  "Failed to find a suitable GPU.\n";
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
--- app/src/main/cpp/vulkan-utils.h
+++ app/src/main/cpp/vulkan-utils.h
@@ -4,18 +4,24 @@
 
 #pragma once
 #include "vulkan_wrapper.h"
+#include <iostream>
 #include <string>
+#include <vector>
 
 class VulkanApplication {
 public:
     VulkanApplication();
     virtual ~VulkanApplication();
     std::string run();
+    std::string createInstance();
     std::string extensionSupport();
+    bool checkValidationSupport();
+    std::string pickPhysicalDevice();
 private:
+    bool enableValidationLayers = false;
     VkInstance vulkanInstance;
-    std::string vulkanInit();
     void vulkanDestroy();
+    const std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
 };
 
 //#ifndef SILDUR_VULKAN_UTILS_H
